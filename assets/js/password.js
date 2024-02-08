67ae22907943c598e7f0102bad21b81d0b1d042c
Alejandro <alejandroidb@gmail.com>
2024-02-08 00:36:46 -0300

    comentarios en archivo password.js

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,16 +1,16 @@
-const verify = document.getElementById('verificar')
-const mensaje = document.querySelector('#mensaje')
+const verify = document.getElementById('verificar') //boton que nos ayuda a confirmar password
+const mensaje = document.querySelector('#mensaje') //variable que mostrara el mensaje de confirmacion
 
-verify.addEventListener('click', function(){
-    let opt1 = document.getElementById('first').value
-    let opt2 = document.getElementById('second').value
-    let opt3 = document.getElementById('third').value
-    let password = opt1+opt2+opt3
-    if (password == "911") {
-        mensaje.innerHTML = "Password 1 correcto"
-    } else if (password == "714") {
-        mensaje.innerHTML = "Password 2 correcto"
-    } else {
-        mensaje.innerHTML = "Password incorrecto"
+verify.addEventListener('click', function(){ //evento a disparar para confirmar
+    let opt1 = document.getElementById('first').value //variable a considerar 1
+    let opt2 = document.getElementById('second').value //variable a considerar 2
+    let opt3 = document.getElementById('third').value //variable a considerar 3
+    let password = opt1+opt2+opt3 //suma de variables para acceder
+    if (password == "911") { //primera condicion
+        mensaje.innerHTML = "Password 1 correcto" //mensaje de confirmacion condicion1
+    } else if (password == "714") { //segunda condicion 
+        mensaje.innerHTML = "Password 2 correcto" //mensaje de confirmacion condicion2
+    } else { //ultima condicion
+        mensaje.innerHTML = "Password incorrecto" //mensaje de confirmacion ultima condicion
     }
 })
